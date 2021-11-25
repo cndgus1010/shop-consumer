b2a1ce831a96cb1d375e35a47817f3eef9bae109
cndggus1010 <chung1838@namver.com>
2021-11-25 11:34:08 +0900

    Navi Axios

--- config-overrides.js
+++ config-overrides.js
@@ -1,17 +1,16 @@
 module.exports = {
   webpack: function (config, env) {
-    config.module.rules.push({
+    /* config.module.rules.unshift({
       test: /\.(png|gif)$/i,
       use: [
         {
           loader: 'url-loader',
           options: {
             limit: 8192,
-            name: '[hash].[ext]',
           },
         },
       ],
-    });
+    }); */
     return config;
   },
 };
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import HeaderCp from './components/inc/HeaderCp';
 import Main from './pages/Main';
 import AuthLogin from './pages/AuthLogin';
 import MyCart from './pages/MyCart';
