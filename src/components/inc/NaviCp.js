7b8802e1b1e5e61569cbc0baa259d7c3c56e5fa2
cndggus1010 <chung1838@namver.com>
2021-11-25 14:03:27 +0900

    Navi Clean

--- src/components/inc/NaviCp.js
+++ src/components/inc/NaviCp.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 import axios from 'axios';
@@ -6,10 +6,17 @@ import axios from 'axios';
 const Wrapper = styled.div``;
 
 const NaviCp = () => {
+  const [tree, setTree] = useState([]);
+
   useEffect(() => {
     const init = async () => {
-      const { data } = await axios.get('http://127.0.0.1:3100/json/tree.json');
-      console.log(data);
+      const { data } = await axios.get('http://127.0.0.1:3100/api/tree');
+      const tree = data[0].children.map((v) => {
+        let children = v.children.map((v2) => ({ id: v2.id, title: v2.text }));
+        return { id: v.id, title: v.text, children };
+      });
+      console.log(tree);
+      setTree(tree);
     };
     init();
     console.log('컴포넌트가 시작됨');
