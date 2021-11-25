a7984cfefa133f8de65e41cb186ec571ba83c63d
cndggus1010 <chung1838@namver.com>
2021-11-25 09:39:22 +0900

    style 완성

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import { store } from './app/store';
+// import { store } from './app/store';
+
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
+import './assets/css/all.min.css';
+import './assets/css/base.css';
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
